b014a429b090f3e01e3602a6d1ae00f9788e8025
ban-tyan <145260845+ban-tyan@users.noreply.github.com>
2024-01-04 15:28:46 +0300

    Update and rename Moving Zeros To The End.c to 5kyu- Moving Zeros To The End.c

--- 5kyu- Moving Zeros To The End.c
+++ 5kyu- Moving Zeros To The End.c
@@ -1,13 +1,3 @@
-/*
-				DESCRIPTION
-
-
-Write an algorithm that takes an array and moves all of the zeros
-to the end, preserving the order of the other elements.
-
-move_zeros(10, int [] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}); // -> int [] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
-*/
-
 #include <stddef.h>
 
 void ft_swap(int *a, int *b)
